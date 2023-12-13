1c493f79b345a63477fdad513308808dc5c669d8
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-13 23:20:18 +0530

    web socket

--- Go_chat_app/server/util/password.go
+++ Go_chat_app/server/util/password.go
@@ -17,3 +17,4 @@ func HashPassword(password string) (string, error) {
 
 func CheckPassword(password string, hashedPassword string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
